d8ae94a6e84b73fc97281d0221030c56f6bdb059
maurobernardo <maurobernardozibane@gmail.com>
2026-09-17 19:03:15 +0200

    Push logo size to near-fill fixed header height

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,7 +10,7 @@ export function Logo({ compact = false }: { compact?: boolean }) {
         width={1450}
         height={1092}
         priority
-        className={compact ? "h-10 w-auto object-contain" : "h-[76px] md:h-[94px] w-auto object-contain"}
+        className={compact ? "h-10 w-auto object-contain" : "h-[78px] md:h-24 w-auto object-contain"}
       />
     </Link>
   );
